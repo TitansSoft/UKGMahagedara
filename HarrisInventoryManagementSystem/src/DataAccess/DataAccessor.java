e8aa0df8c48700af61e17811499a521a4c0702a5
MalakaGL <glmalaka@gmail.com>
2013-09-29 01:06:49 +0530

    User Authentication

--- HarrisInventoryManagementSystem/src/DataAccess/DataAccessor.java
+++ HarrisInventoryManagementSystem/src/DataAccess/DataAccessor.java
@@ -1,5 +1,9 @@
 package DataAccess;
 
+import java.nio.CharBuffer;
+import java.nio.charset.Charset;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
@@ -7,6 +11,7 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import viewL.Gui.LogIn;
 
 public class DataAccessor {
 
@@ -39,5 +44,55 @@ public class DataAccessor {
             Logger.getLogger(DataAccessor.class.getName()).log(Level.SEVERE, null, ex);
         }
         return password;
+    }
+    
+    public String encrypt(char[] password) {
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
+            byte[] bytes = Charset.forName("UTF-8").encode(CharBuffer.wrap(password)).array();
+            md.update(bytes);
+
+            byte byteData[] = md.digest();
+
+            //convert the byte to hex format method 1
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < byteData.length; i++) {
+                sb.append(Integer.toString((byteData[i] & 0xff) + 0x100, 16).substring(1));
+            }
+
+            //convert the byte to hex format method 2
+            StringBuilder hexString = new StringBuilder();
+            for (int i = 0; i < byteData.length; i++) {
+                String hex = Integer.toHexString(0xff & byteData[i]);
+                if (hex.length() == 1) {
+                    hexString.append('0');
+                }
+                hexString.append(hex);
+            }
+
+            return hexString.toString();
+
+        } catch (NoSuchAlgorithmException ex) {
+            Logger.getLogger(LogIn.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
+    }
+
+    public String checkPassword(String enteredPassword) {
+        String userName = null;
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            connect = DriverManager.getConnection("jdbc:mysql://localhost/UKG?"
+                    + "user=root&password=123");
+            statement = connect.createStatement();
+            resultSet = statement.executeQuery("SELECT UserID from user WHERE Password = '" + enteredPassword + "'");
+            if (resultSet.next()) {
+                userName = resultSet.getString("UserID");
+                return userName;
+            }
+        } catch (SQLException | ClassNotFoundException ex) {
+            Logger.getLogger(DataAccessor.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return userName;
     }
 }
--- HarrisInventoryManagementSystem/src/viewL/Gui/LogIn.java
+++ HarrisInventoryManagementSystem/src/viewL/Gui/LogIn.java
@@ -255,7 +255,7 @@ public class LogIn extends javax.swing.JFrame {
         String passwordStored;
 
         passwordStored = connection.getPassword(userName);
-        String password = encrypt(passwordEntered);
+        String password = connection.encrypt(passwordEntered);
 
         //connection.createUser("user",encrypt(this.txtfldPassword.getPassword()));
         System.out.println("Entered : " + password);
@@ -283,38 +283,6 @@ public class LogIn extends javax.swing.JFrame {
         } else {
             JOptionPane.showMessageDialog(this, "Login Failure.\nInvalid Password");
         }
-    }
-
-    private String encrypt(char[] password) {
-        try {
-            MessageDigest md = MessageDigest.getInstance("SHA-256");
-            byte[] bytes = Charset.forName("UTF-8").encode(CharBuffer.wrap(password)).array();
-            md.update(bytes);
-
-            byte byteData[] = md.digest();
-
-            //convert the byte to hex format method 1
-            StringBuilder sb = new StringBuilder();
-            for (int i = 0; i < byteData.length; i++) {
-                sb.append(Integer.toString((byteData[i] & 0xff) + 0x100, 16).substring(1));
-            }
-
-            //convert the byte to hex format method 2
-            StringBuilder hexString = new StringBuilder();
-            for (int i = 0; i < byteData.length; i++) {
-                String hex = Integer.toHexString(0xff & byteData[i]);
-                if (hex.length() == 1) {
-                    hexString.append('0');
-                }
-                hexString.append(hex);
-            }
-
-            return hexString.toString();
-
-        } catch (NoSuchAlgorithmException ex) {
-            Logger.getLogger(LogIn.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        return null;
     }
 
     public void readLogIn() {
