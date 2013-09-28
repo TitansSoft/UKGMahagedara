2390dee4bcff10be94a7a595127ee1ce330c893d
MalakaGL <glmalaka@gmail.com>
2013-09-28 22:05:07 +0530

    Log in successful. User Name : user Password : 123

--- HarrisInventoryManagementSystem/src/bussinessLogicL/Authenticator/userAuthenticator.java
+++ HarrisInventoryManagementSystem/src/bussinessLogicL/Authenticator/userAuthenticator.java
@@ -1,24 +1,35 @@
 package bussinessLogicL.Authenticator;
 
 import DataAccess.DataAccessor;
+import javax.swing.JOptionPane;
 import viewL.Users.User;
 
 public class userAuthenticator {
 
-    private boolean testAuthentication = false;
+    private static boolean testAuthentication = false;
 
-    public boolean authenticated(String name, String passward) {
+    public static void main(String args[]) {
+        boolean a = checkForAdmin("user");
+        System.out.println("a = " + a);
+    }
+
+    public static boolean authenticated(String name, String passward) {
         DataAccessor da = new DataAccessor();
-        String storedPassword = da.getPassword(name);
-        this.testAuthentication = storedPassword.equals(passward);
+        if (da.getPassword(name) != null) {
+            String storedPassword = da.getPassword(name);
+            userAuthenticator.testAuthentication = storedPassword.equals(passward);
+        }
         return testAuthentication;
     }
 
-    public boolean checkForAdmin(String name, String password) {
+    public static boolean checkForAdmin(String userName) {
         boolean result = false;
-        User user = User.getUser(name);
+        String password = JOptionPane.showInputDialog(
+                "Please input an administrator password to proceed",
+                JOptionPane.QUESTION_MESSAGE);
+        User user = User.getUser(userName);
 
-        if (authenticated(name, password) && user.getAuthority().equals("Admin")) {
+        if (authenticated(userName, password) && user.getAuthority().equals("Admin")) {
             result = true;
         }
 
