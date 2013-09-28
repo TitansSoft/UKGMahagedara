08d8c4fb6bffec37053f39c0294ba056e9ddc4f2
MalakaGL <glmalaka@gmail.com>
2013-09-28 12:44:03 +0530

    Log in successful. User Name : user Password : 123

--- HarrisInventoryManagementSystem/src/bussinessLogicL/Authenticator/userAuthenticator.java
+++ HarrisInventoryManagementSystem/src/bussinessLogicL/Authenticator/userAuthenticator.java
@@ -1,14 +1,27 @@
 package bussinessLogicL.Authenticator;
+
+import DataAccess.DataAccessor;
+import viewL.Users.User;
+
 public class userAuthenticator {
-	private boolean testAuthentication = false;
-	
-	public boolean authenticated(String name, String passward){			
-		this.testAuthentication = this.retrievePassward(name) == passward;	
-		return testAuthentication;
-	}
-	
-	private String retrievePassward(String name){
-		String passward = null;
-		return passward;
-	}
+
+    private boolean testAuthentication = false;
+
+    public boolean authenticated(String name, String passward) {
+        DataAccessor da = new DataAccessor();
+        String storedPassword = da.getPassword(name);
+        this.testAuthentication = storedPassword.equals(passward);
+        return testAuthentication;
+    }
+
+    public boolean checkForAdmin(String name, String password) {
+        boolean result = false;
+        User user = User.getUser(name);
+
+        if (authenticated(name, password) && user.getAuthority().equals("Admin")) {
+            result = true;
+        }
+
+        return result;
+    }
 }
