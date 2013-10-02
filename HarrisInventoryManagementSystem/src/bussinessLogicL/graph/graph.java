ac95c4a637e051bd8b82276e851158bbb0e93bb2
MalakaGL <glmalaka@gmail.com>
2013-10-02 11:07:39 +0530

    User Authentication

--- HarrisInventoryManagementSystem/src/bussinessLogicL/graph/graph.java
+++ HarrisInventoryManagementSystem/src/bussinessLogicL/graph/graph.java
@@ -3,31 +3,54 @@ package bussinessLogicL.graph;
 public class graph {
 
     public static void main(String[] args) {
-        double x[] = {1, 2, 4, 5};
+        double x[] = {1, 3, 8, 9};
         double y[] = {1, 2, 4, 5};
-        System.out.println("Answer = " + getInterpolatedPoint(x, y, 3.0));
+        double [] equation = getMandC(x, y);
+        double m = equation [ 0 ];
+        double c = equation[1];
+        
+        for(int xi = 0 ; xi < 10 ;xi++){
+            double yi = m * xi + c;
+            System.out.println("Y at x = " + xi + " is " + yi);
+        }
     }
 
-    static double getInterpolatedPoint(double x[], double y[], double x0) {
+    static double [] getMandC(double x[], double y[]) {
         double answer = 0;
-        double L[] = new double[x.length];
-
-        for (int i = 0; i < x.length; i++) {
-            L[i] = 1;
+        double xBar;
+        double yBar ;
+        double temp = 0;
+        double bOne;
+        double bNote;
+        double [] mAndC ;
+                
+        for (int i = 0 ; i < x.length ; i++ ){
+            answer += x[i];
         }
 
-        for (int i = 0; i < x.length; i++) {
-            for (int j = 0; j < x.length; j++) {
-                if (i != j) {
-                    L[i] *= ((x0 - x[j]) / (x[i] - x[j]));
-                }
-            }
+        xBar = answer / x.length;
+        answer = 0;
+        
+        for (int i = 0 ; i < y.length ; i++ ){
+            answer += y[i];
         }
-
-        for (int i = 0; i < x.length; i++) {
-            System.out.println(L[i] + " " + y[i]);
-            answer += (L[i] * y[i]);
+        
+        yBar = answer / y.length;
+        answer = 0;
+        
+        for(int i = 0; i < x.length;i++){
+            answer += (x[i] - xBar )*(y[i] - yBar);
         }
-        return answer;
+        
+        for(int i = 0; i < x.length;i++){
+            temp += (x[i] - xBar )*(x[i] - xBar );
+        }
+        
+        bOne = answer / temp;
+        bNote = yBar - ( bOne * xBar );
+        mAndC = new double[2];
+        mAndC[0] = bOne;
+        mAndC[1] = bNote;
+        return mAndC;
     }
 }
